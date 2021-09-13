8da17755770537d98dfdaa2ac1eaa7417bbfa55d
animesaga-tech <75627574+animesaga-tech@users.noreply.github.com>
2021-09-13 17:30:20 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 from flask import Flask,Response,request,jsonify
 import requests
-import re
 app=Flask(__name__)
-def headers(response):
-	response=jsonify({"content":content})
-	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-	response.headers.add('Access-Control-Allow-Origin', '*')
-	response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-	return response
-def get_mp4link(episodeid):
-	url="https://streamani.net/streaming.php?id="+episodeid
-	ssn=requests.Session()
-	data=ssn.get(url).text
-	link=data.split(' data-provider="serverwithtoken" data-video="')[1].split('">Multiquality Server</li>')[0]
-	data2=ssn.get(link).text
-	mp4s=re.findall("https://.*?mp4",data2)
-	print(mp4s)
-	
-
-
-
 @app.route('/<path:path>')
 def catch_all(path):
-	if request.args.get('episodeid') is None:
-		return "This is the homepage please add episodeid paramater"
+	if request.args.get('url') is None:
+		return "This is the homepage please add url parameter and replace the & with ^ character"
 	else:
-		episodeid=request.args.get('episodeid')
-		get_mp4link(episodeid)
-
-app.run(debug=True) 
+		url=request.args['url'].replace("^","&")
+		content=requests.get(url)
+		try:
+			content=content.json()
+			response=jsonify(content)
+		except:
+			content=content.text
+			response=jsonify({"content":content})
+		response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+		response.headers.add('Access-Control-Allow-Origin', '*')
+		response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+		return response
